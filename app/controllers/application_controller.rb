0bcadc7f2876ca0a31bee60ba83bcb07f548d285
Josh Sample <joshuawsample@gmail.com>
2018-03-24 14:52:34 -0400

    Initial commit hartl lab

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡Hola, Mundo!"
+  end
+
+  def goodbye
+    render html: "¡Adios, Mundo!"
+  end
+
+
+end
